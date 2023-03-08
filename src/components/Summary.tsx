9e6b390e143100e7070a340b5559384f7bf5eb7e
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2023-03-08 02:46:05 -0800

    summary done

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -7,30 +7,51 @@ interface SummaryProps {
 }
 
 const Summary = ({ summary, setSummary }: SummaryProps) => {
+  const { plan, yearly, addOns } = summary;
+
+  const total = yearly
+    ? plan.yearlyPrice + addOns.reduce((acc, curr) => acc + curr.yearlyPrice, 0)
+    : plan.monthlyPrice +
+      addOns.reduce((acc, curr) => acc + curr.monthlyPrice, 0);
+
   return (
-    <section className="summary">
-      <li className="summary__li">
-        <div className="summary__flex">
-          <div className="summary__name">
-            {summary.plan.name} ({summary.yearly ? "Yearly" : "Monthly"})
+    <>
+      <section className="summary">
+        <div className="summary__top">
+          <div className="summary__flex">
+            <div className="summary__name">
+              {summary.plan.name} ({summary.yearly ? "Yearly" : "Monthly"})
+            </div>
+            <div className="summary__change">Change</div>
           </div>
-          <div className="summary__change">Change</div>
+          <div className="summary__price">
+            ${yearly ? plan.yearlyPrice : plan.monthlyPrice}/
+            {yearly ? "yr" : "mo"}
+          </div>
         </div>
-        <div className="summary__price">${summary.plan.monthlyPrice}/mo</div>
-      </li>
-      {summary.addOns.map((addOn) => (
-        <li className="summary__add-on">
-          <div className="summary__add-on--name">{addOn.name}</div>
-          <div className="summary__add-on--price">
-            +${summary.yearly ? addOn.yearlyPrice : addOn.monthlyPrice}/mo
-          </div>
-        </li>
-      ))}
+        {addOns.length > 0 && (
+          <ul className="summary__bottom">
+            {summary.addOns.map((addOn) => (
+              <li className="summary__add-on">
+                <div className="summary__add-on--name">{addOn.name}</div>
+                <div className="summary__add-on--price">
+                  +${summary.yearly ? addOn.yearlyPrice : addOn.monthlyPrice}/
+                  {yearly ? "yr" : "mo"}
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </section>
       <div className="summary__total">
-        <div className="summary__total--label"></div>
-        <div className="summary__total--amount"></div>
+        <div className="summary__total--label">
+          Total (per {yearly ? "year" : "month"})
+        </div>
+        <div className="summary__total--amount">
+          +${total}/{yearly ? "yr" : "mo"}
+        </div>
       </div>
-    </section>
+    </>
   );
 };
 
